1835e01c7d2c7dada85a238df9abc140c1ebe175
maartin0 <maartin00000@gmail.com>
2026-06-13 16:15:30 +0100

    use uart8 instead of uart1 (todo: also mirror debug output over USART1->RS232)

--- src/SUFST/Src/config.c
+++ src/SUFST/Src/config.c
@@ -169,7 +169,7 @@ static const config_t config_instance = {
             .stack_size = 1024,
         },
         .min_level = LOG_LEVEL_DEBUG,
-        .uart = &huart1
+        .uart = &huart8
     },
     .rtos = {
         .rtcan_s_priority = 3,
